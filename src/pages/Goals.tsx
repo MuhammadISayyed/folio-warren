583c78583fc272ab7d4546d990e30adaeb54433e
Muhammad I. Sayyed <muhammadisayyed@gmail.com>
2024-02-07 18:13:38 +0200

    Layout the goals page

--- src/pages/Goals.tsx
+++ src/pages/Goals.tsx
@@ -1,26 +1,49 @@
+// TODOS
+// 1. Separate prioritized from deprioritized goals
+// 2. Sort goals by date
+
 import { Link } from 'react-router-dom'
 import { supabase } from '../../lib/supabaseClient'
 import { useEffect, useState } from 'react'
 import AppNav from '../components/AppNav'
+import styles from '../styles/goals.module.css'
 
 type GoalsProps = {
   userId: string | undefined
 }
 
 const Goals = ({ userId }: GoalsProps) => {
-  const [goals, setGoals] = useState<{ id: string; title: string }[] | null>([])
+  const [prioritizedGoals, setPrioritizedGoals] = useState<
+    { id: string; title: string; description: string }[] | null
+  >([])
+  const [deprioritizedGoals, setDeprioritizedGoals] = useState<
+    { id: string; title: string; description: string }[] | null
+  >([])
 
   useEffect(() => {
     const fetchGoals = async () => {
-      const { data, error } = await supabase.from('goals').select().eq('user_id', userId)
+      // Fetch prioritized goals
+      const { data: prioritized, error: errWithPrioritized } = await supabase
+        .from('goals')
+        .select()
+        .eq('user_id', userId)
+        .eq('prioritized', true)
+        .order('updated_at', { ascending: true })
+      if (errWithPrioritized) console.log(errWithPrioritized)
+      if (prioritized) {
+        setPrioritizedGoals(prioritized)
+      }
 
-      if (error) console.log(error)
-
-      // console.log(user)
-      console.log(data)
-
-      if (data) {
-        setGoals(data)
+      // Fetch deprioritized goals
+      const { data: deprioritized, error: errWithDeprioritized } = await supabase
+        .from('goals')
+        .select()
+        .eq('user_id', userId)
+        .eq('prioritized', false)
+        .order('updated_at', { ascending: true })
+      if (errWithDeprioritized) console.log(errWithDeprioritized)
+      if (deprioritized) {
+        setDeprioritizedGoals(deprioritized)
       }
     }
     fetchGoals()
@@ -29,11 +52,33 @@ const Goals = ({ userId }: GoalsProps) => {
   return (
     <div>
       <AppNav />
-      {goals?.map((goal) => (
-        <Link to={`/goals/${goal.id}`} key={goal.id}>
-          {goal.title}
-        </Link>
-      ))}
+      <div>
+        <h2>Prioritized Goals</h2>
+        <div className={styles.goalsContainer}>
+          {prioritizedGoals?.map((goal) => (
+            <div>
+              <Link to={`/goals/${goal.id}`} className={styles.goalLink} key={goal.id}>
+                <span className={styles.goalTitle}>{goal.title}</span>
+                <span className={styles.goalDescription}>{goal.description}</span>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div>
+        <h2>Deprioritized Goals</h2>
+        <div className={styles.goalsContainer}>
+          {deprioritizedGoals?.map((goal) => (
+            <div>
+              <Link to={`/goals/${goal.id}`} className={styles.goalLink} key={goal.id}>
+                <span className={styles.goalTitle}>{goal.title}</span>
+                <span className={styles.goalDescription}>{goal.description}</span>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
